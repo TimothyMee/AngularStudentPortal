ec447177fbb274a56978456c410e1a6ce22ed2b1
Femi Akinbode <femi661@gmail.com>
2019-06-11 18:57:53 +0100

    Add project files.

---
+++ AngularWithCore/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularWithCore.Controllers
+{
+    [Route("api/[Controller")]
+    public class TestController : Controller
+    {
+        [HttpGet("/api/values")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "a", "b", "c", "d" };
+        }
+    }
+}
