51c725f6c6c53c32556b2c930db3913ee755d347
JesuT <tww.jesutomi@gmail.com>
2019-06-13 09:41:12 +0100

    loginapi

---
+++ API/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using API.Models;
+using Premier.EntityClasses;
+using AppFramework;
+
+namespace API.Controllers
+{
+    public class LoginController : ApiController
+    {
+        // POST: api/Login
+        [Route("Api/Login/UserLogin")]
+        [HttpPost]
+        public Response Login(Login Lg)
+        {
+            Students students = new Students();
+            var student = Util.studentLogin(Lg.Username, Lg.Password, true); ;
+            if (student == null)
+                return new Response { Status = "Invalid", Message = "Invalid User." };
+            else
+                return new Response { Status = "Success", Message = Lg.Username };
+        }
+    }
+}
---
+++ API/Util.cs
@@ -0,0 +1,44 @@
+﻿using AppFramework.AppClasses;
+using AppFramework.Linq;
+using Premier.EntityClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API
+{
+    public static class Util
+    {
+        internal static Students studentLogin(String matricno, String password, bool withoutpassword = false)
+        {
+            var studentexists = new QueryableEntity<Students>().Any(x => x.MatricNo == matricno);
+
+            if (!studentexists)
+            {
+                return null;
+            }
+
+            var latestcompany = (from sp in new QueryableEntity<StudentProgrammes>() where sp.MatricNo == matricno orderby sp.Session descending select sp.Company).ToList().AppFirst();
+
+            if (latestcompany == null)
+            {
+                throw new Exception("StudentProgramme Not Found for " + matricno);
+            }
+
+            String passwordString = DatabaseHandler.DefaultDatabaseHandlerObject.getPasswordString(password);
+            //String passwordString2 = AppFramework.AppClasses.Util.md5(password);
+            var students = (from student in new QueryableEntity<Students>()
+                            join stdprog in new QueryableEntity<StudentProgrammes>()
+                            on student.MatricNo equals stdprog.MatricNo
+                            where student.MatricNo == matricno &&
+                            (student.Password == passwordString || withoutpassword) &&
+                            student.Company == latestcompany &&
+                            stdprog.Company == latestcompany
+                            select student).ToList().AppFirst();
+
+            return students;
+        }
+
+    }
+}
